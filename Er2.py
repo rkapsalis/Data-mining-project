c09c3a2500e3edb6a32b9b5685620b288f1ab4e1
ZIZIMOZ TRABARiZ <60177254+mrZisiz@users.noreply.github.com>
2020-04-25 17:37:39 +0300

    Add files via upload

--- Er2.py
+++ Er2.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from random import randrange
 from matplotlib import pyplot as plt
+import numpy as np
 
 data = pd.read_csv (r'winequality-red.csv')
 
@@ -35,11 +36,9 @@ median = median/not_erased
 for index in nancol:
     X_median[index][pH] = median
     
-sc = StandardScaler()
-X_median = sc.fit_transform(X_median)
 
-#Για χωρις στηλη εξεταζουμε με X_noph -> y_train για train και με y_noph -> y_test //Μια λιγοτερη είσοδος
-#Για τον μέσο όρο εξετάζουμε με X_median -> y_train για train και με X_test -> y_test
+
+
 
 for index in nancol:
     X_kmeans[index][pH] = None
@@ -56,7 +55,47 @@ for index in nancol:
 # plt.xlabel('Number of clusters')
 # plt.ylabel('WCSS')
 # plt.show()
+#Αυτα ειναι για να δειξουμε γιατι 4
+
 
 kmeans = KMeans(n_clusters=4, init='k-means++', max_iter=300, n_init=10, random_state=0)
-pred_y = kmeans.fit(X_kplot)
-print(pred_y)
+centers = kmeans.fit(X_kplot).cluster_centers_
+
+clusters = []
+for i in range(len(centers)):
+    clusters.append([])
+for i in range(len(X_kplot)):
+    dist = []
+    for j in range(len(centers)):
+        dist.append([np.linalg.norm(X_kplot[i]-centers[j]),j])
+    dist.sort()
+    clusters[dist[0][1]].append(i)
+
+median = []
+for i in range(len(clusters)):
+    median.append(0)
+
+clust_num = 0
+for cluster in clusters:
+    not_erased=0
+    for index in cluster:
+        if X_kmeans[index][pH] is not None:
+            median[clust_num] += X_kmeans[index][pH]
+            not_erased+=1
+    if not_erased != 0:
+        median[clust_num] /=not_erased
+    for index in cluster:
+        if X_kmeans[index][pH] is None:
+            X_kmeans[index][pH] = median[clust_num]
+    clust_num+=1
+
+
+sc = StandardScaler()
+X_median = sc.fit_transform(X_median)
+X_noph = sc.fit_transform(X_noph)
+X_kmeans = sc.fit_transform(X_kmeans)
+
+
+#Για χωρις στηλη εξεταζουμε με X_noph -> y_train για train και με y_noph -> y_test //Μια λιγοτερη είσοδος
+#Για τον μέσο όρο εξετάζουμε με X_median -> y_train για train και με X_test -> y_test
+#Για τους γειτονες εξετάζουμε με X_kmeans -> y_train για train και με X_test -> y_test
